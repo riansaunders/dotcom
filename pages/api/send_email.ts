47fdf4ccb895c36285d36b5bdb275f63ef1efb1a
riansaunders <rian@botmw.com>
2022-04-01 11:50:33 -0400

    TIPP

--- pages/api/send_email.ts
+++ pages/api/send_email.ts
@@ -34,6 +34,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         ],
       },
     })
-    .then(() => res.status(200).end())
+    .then((r) => {
+      console.log(r);
+      console.log(req.body);
+      res.status(200).end();
+    })
     .catch(() => res.status(500).end());
 };
